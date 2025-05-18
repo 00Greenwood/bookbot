3243af01c8ce329da9cb367491bad09bda5a6d45
00Greenwood <marcus_greenwood@hotmail.co.uk>
2025-05-18 19:30:01 +0200

    Update main.py to read a book

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
+def main():
+    file_path = "./books/frankenstein.txt"
+    book_text = get_book_text(file_path)
+    print(book_text)
+
+
+main()
