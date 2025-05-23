2fe28e502bb388e6b4b99ccbdbf2ab3b1950d93c
00Greenwood <marcus_greenwood@hotmail.co.uk>
2025-05-23 22:26:03 +0200

    Update main to use arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_freq_chars, get_sorted_chars
+import sys
 
 
 def get_book_text(file_path):
@@ -6,10 +7,19 @@ def get_book_text(file_path):
         return f.read()
 
 
+def get_book_file_path():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    file_path = "./books/frankenstein.txt"
+    book_file_path = get_book_file_path()
+    file_path = f"./{book_file_path}"
+    print(f"Analyzing book found at {file_path}...")
     book_text = get_book_text(file_path)
     num_words = get_num_words(book_text)
     print("----------- Word Count ----------")
